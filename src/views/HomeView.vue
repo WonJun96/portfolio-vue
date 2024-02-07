fd6b2b36bc41b3790411bcdd0ff1317742b0e4e1
Wonjun96 <wonjun0824@gmail.com>
2024-02-07 10:16:18 +0900

    [24.02.07] 컴퍼넌트 완료 🧑‍💻

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,23 @@
+<script setup>
+import ContactSection from "@/components/ContactSection.vue";
+import HeaderSection from "@/components/HeaderSection.vue";
+import FooterSection from "@/components/FooterSection.vue";
+import IntroSection from "@/components/IntroSection.vue";
+import SkipSection from "@/components/SkipSection.vue";
+import SiteSection from "@/components/SiteSection.vue";
+import PortSection from "@/components/PortSection.vue";
+import SkillSection from "@/components/SkillSection.vue";
+</script>
+
 <template>
-  <main>
-    Home
+  <SkipSection />
+  <HeaderSection />
+  <main id="main" role="main">
+    <IntroSection />
+    <SkillSection />
+    <SiteSection />
+    <PortSection />
+    <ContactSection />
   </main>
+  <FooterSection />
 </template>
